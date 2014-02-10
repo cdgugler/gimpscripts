a8b1ab5f16539c81d75c677c8c03795046b153eb
Cory Gugler <cory.gugler@gmail.com>
2014-02-10 00:21:19 -0600

    Added required images for scripts, updated readme

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ Mirrors image across horizontal axis then vertical axis, creating tileable image
 
 ### Generate Header Images
 Convenience script for Addlime.com. Converts scanned image into multiple horizontal tileable header images.
+
+### Crayon-fx
+A set of batch gimp scripts and bash scripts for applying a wax crayon look to a video
--- crayon_filter_pastel.sh
+++ crayon_filter_pastel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Extract 9 frames per second from video
-ffmpeg -i $1 -r 6 -s hd720 image%3d.png
+ffmpeg -i $1 -r 9 -s hd720 image%3d.png
 
 # Debug
 # ffmpeg -i IMG_1948.MOV -r 9 -t 4 -ss 00:00:07 -s hd720 image%3d.png
@@ -11,7 +11,7 @@ gimp -i -b '(crayon-fx-pastel "*.png")' -b '(gimp-quit 0)'
 
 
 # And make it a video again
-ffmpeg -r 6 -i image%03d.png.svg.png -vcodec qtrle -r 24 -pix_fmt rgb24 ${1}-crayon.mov
+ffmpeg -r 9 -i image%03d.png.png -vcodec qtrle -r 24 -pix_fmt rgb24 ${1}-crayon.mov
 
 # Clean up dir
 rm ./*.png
