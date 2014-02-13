ef8254a8288ae2295d40578cf576bbe801d5624f
Cory Gugler <cory.gugler@gmail.com>
2014-02-12 20:15:51 -0600

    Added texture

--- crayon_filter_pastel.sh
+++ crayon_filter_pastel.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 # Extract 9 frames per second from video
-ffmpeg -i $1 -r 9 -s hd720 image%3d.png
+# ffmpeg -i $1 -r 9 -s hd720 image%3d.png
 
-# Debug
-# ffmpeg -i IMG_1948.MOV -r 9 -t 4 -ss 00:00:07 -s hd720 image%3d.png
+############ Debug #############
+# ffmpeg -i IMG_1949.MOV -r 9 -t 12 -ss 00:00:07 -s hd720 image%3d.png
+# or
+ffmpeg -i $1 -r 9 -t 12 -ss 00:00:00 -s hd720 image%3d.png
+############ Debug #############
 
+echo '***************** Run Gimp Script ***********************'
+echo '*********************************************************'
 # Gimp script to add crayon texture to all images
-gimp -i -b '(crayon-fx-pastel "*.png")' -b '(gimp-quit 0)'
+gimp -i -b "(crayon-fx-pastel \"*.png\" $2 ${3})" -b '(gimp-quit 0)'
 
 
 # And make it a video again
-ffmpeg -r 9 -i image%03d.png.png -vcodec qtrle -r 24 -pix_fmt rgb24 ${1}-crayon.mov
+ffmpeg -r 9 -i image%03d.png -vcodec qtrle -r 24 -pix_fmt rgb24 ${1}-crayon.mov
 
 # Clean up dir
 rm ./*.png
